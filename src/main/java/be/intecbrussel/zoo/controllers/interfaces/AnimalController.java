3b4ffbcf0efeafb3e007da6610a9b6df68f38d8f
PearlDS <62283615+PearlDS@users.noreply.github.com>
2022-09-11 23:32:23 +0200

    Update AnimalController.java

--- src/main/java/be/intecbrussel/zoo/controllers/interfaces/AnimalController.java
+++ src/main/java/be/intecbrussel/zoo/controllers/interfaces/AnimalController.java
@@ -1,9 +1,10 @@
 package be.intecbrussel.zoo.controllers.interfaces;
 
+import org.springframework.ui.Model;
 import be.intecbrussel.zoo.data.Country;
 
 public interface AnimalController {
 
-    String seeAllAnimalsByCountry(Country country);
+    String seeAllAnimalsByCountry(Model model, Country country);
     String addAnimal(String animalName, String countryName);
 }
